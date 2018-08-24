52936fe9e03ae99e8f314c670eceac3b8e642e5a
Andy Taylor <Theador0691@gmail.com>
2018-08-24 13:55:43 +0400

    Finished the quiz controller now onto the heads up display!

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
     
     //Place your instance variables here
     
-    
+    let allQuestion = QuestionBank()
+    var pickedAnswer = false
+    var questionNumber = 0
+    var score = 0
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
@@ -20,32 +23,58 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateUI()
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1{
+            pickedAnswer = true
+            } else {
+            pickedAnswer = false
+        }
+        checkAnswer()
+        nextQuestion()
     }
     
     
     func updateUI() {
-      
+      progressLabel.text = String(questionNumber+1)
+      progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
+      scoreLabel.text = String(score)
+      questionLabel.text = allQuestion.list[questionNumber].questionText
+        
+
     }
     
 
     func nextQuestion() {
-        
+        if questionNumber == allQuestion.list.count - 1 {
+            let alert = UIAlertController(title: "You did it!", message: "You did it, you completed the quiz", preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+            self.startOver()
+            }
+            alert.addAction(restartAction)
+            present(alert, animated: true, completion: nil)
+        }else{
+            questionNumber += 1
+            updateUI()
+        }
+  
     }
     
     
     func checkAnswer() {
-        
+        if pickedAnswer == allQuestion.list[questionNumber].answer {
+            score += 1
+        }
     }
     
     
     func startOver() {
-       
+        questionNumber = 0
+        score = 0
+        updateUI()
     }
     
 
